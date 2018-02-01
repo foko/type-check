1d3012aadcae9700ee2836f534687c5abd19edfc
Karim Alibhai <karim@foko.co>
2018-02-01 10:11:41 -0500

    Add support for regex and optionals

--- README.md
+++ README.md
@@ -24,6 +24,34 @@ add(1) // throws TypeError
 // etc.
 ```
 
+## Available Types
+
+ - undefined
+ - object
+ - boolean
+ - number
+ - string
+ - symbol
+ - function
+ - null
+ - array
+ - regexp
+
+*If you would like to make any of these "optional" (i.e. any above value, or undefined - simply append "?" to the end)*
+
+### Required vs. Optional
+
+```javascript
+function add (a, b) {
+  typeCheck('number', a)
+  typeCheck('number?', b)
+
+  return a + (b || 1)
+}
+
+add(1) // this does not error out anymore, now returns 2
+```
+
 ## License
 
 Licensed under MIT license.
--- index.js
+++ index.js
@@ -13,6 +13,10 @@ function typeOf (value) {
     return 'array'
   }
 
+  if (typeof value === 'object' && value instanceof RegExp) {
+    return 'regexp'
+  }
+
   return typeof value
 }
 
@@ -21,7 +25,20 @@ function typeOf (value) {
  * on typeOf() value.
  */
 function typeAssert (type, value) {
+  const isOptional = type[type.length - 1] === '?'
+
+  // trim the '?'
+  if (isOptional) {
+    type = type.substr(0, type.length - 1)
+  }
+
   if (typeOf(value) !== type) {
+    // if its optional, then don't fail for undefined or null
+    if (isOptional && (value === undefined || value === null)) {
+      return
+    }
+
+    // otherwise throw error
     throw new TypeError(`Unexpected value of type: ${type}`)
   }
 }
@@ -39,6 +56,7 @@ const PRIMITIVES = {
   // special typeof checks
   null: 1,
   array: 1,
+  regexp: 1,
 }
 
 // checks that cannot be completed through usual typeOf check
@@ -49,7 +67,11 @@ const TYPES = {
 }
 
 function typeCheck (type, value) {
-  typeAssert('string', type)
+  if (typeof type === 'string') {
+    type = [type]
+  }
+
+  typeAssert('array', type)
 
   /**
    * Primitives are easy. Just use typeof.
